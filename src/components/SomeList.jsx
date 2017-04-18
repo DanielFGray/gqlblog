e1e37c6ea03dd3d98a952a4b35422ae7d4768d23
DanielFGray <DanielFGray@gmail.com>
2017-04-18 00:47:59 -0500

    fixing things

--- src/components/SomeList.jsx
+++ src/components/SomeList.jsx
@@ -4,22 +4,22 @@ import React, { Component } from 'react'
 import styles from './list.sss'
 import ItemEntry from './ItemEntry'
 
-/* eslint-disable react/prop-types */
-
 class SomeList extends Component {
   state = {
-    list: [1, 2, 3],
+    list: [ 1, 2, 3 ],
   }
 
-  addItem = (num: number) =>
-    this.setState(prev => ({ list: prev.list.concat(num) }), () => console.log(this.state.list))
+  addItem = (num: number) => {
+    this.setState(prev => ({ list: prev.list.concat(num) }))
+  }
 
   render() {
     return (
       <div className={styles.list}>
         <ItemEntry addItem={this.addItem} />
         <ul>
-          {this.state.list.map(e => <li key={e}>{e} * {e} = {e * e}</li>)}
+          {this.state.list.map(e =>
+            <li key={e}>{e} * {e} = {e * e}</li>)}
         </ul>
       </div>
     )
