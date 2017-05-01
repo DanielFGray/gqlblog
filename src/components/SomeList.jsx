a13decd1abdc2c15d556494f897cf17a81524a9a
DanielFGray <DanielFGray@gmail.com>
2017-05-01 03:27:01 -0500

    bike shedding

--- src/components/SomeList.jsx
+++ src/components/SomeList.jsx
@@ -17,13 +17,16 @@ class SomeList extends Component {
     return (
       <div className={styles.list}>
         <ItemEntry addItem={this.addItem} />
-        <ul>
-          {this.state.list.map(e =>
-            <li key={e}>{e} * {e} = {e * e}</li>)}
-        </ul>
+        <ListView list={this.state.list} />
       </div>
     )
   }
 }
+
+const ListView = ({ list }: { list: Array<number> }) =>
+  <ul>
+    {list.map(e =>
+      <li key={e}>{e} * {e} = {e * e}</li>)}
+  </ul>
 
 export default SomeList
