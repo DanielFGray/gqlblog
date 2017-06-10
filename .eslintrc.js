6a8d65aef48083402dcbe1cf91db8b9697c87698
DanielFGray <DanielFGray@gmail.com>
2017-06-09 22:47:08 -0500

    little bit more lax eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,21 +13,33 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'no-unexpected-multiline': 'error',
-    'no-nested-ternary': 0,
-    'space-unary-ops': [
-      2, {
-        words: true,
-        nonwords: false,
-        overrides: {
-          '!': true,
-        },
+    'no-nested-ternary': 'off',
+    'space-unary-ops': ['error', {
+      words: true,
+      nonwords: false,
+      overrides: {
+        '!': true,
       },
-    ],
-    'no-unused-vars': [
-      'error', {
-        argsIgnorePattern: '^_$',
-      },
-    ],
+    }],
+    'no-unused-vars': ['error', {
+      argsIgnorePattern: '^_$',
+      vars: 'all',
+      args: 'after-used',
+      ignoreRestSiblings: true,
+    }],
+    'arrow-parens': ['error', 'as-needed'],
+    'react/prop-types': ['warn', {
+      ignore: [],
+      customValidators: [],
+      skipUndeclared: false,
+    }],
+    'react/forbid-prop-types': ['warn', {
+      forbid: [
+        'any',
+        'array',
+        'object',
+      ],
+    }],
     'react/sort-comp': ['error', {
       order: [
         'type-annotations',
