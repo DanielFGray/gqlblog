de5c0da5b07d441b01d76fcf9758d5062f887eb0
DanielFGray <DanielFGray@gmail.com>
2018-08-05 16:26:43 -0500

    jk don't use babel in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies,global-require */
 
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const devMode = process.env.NODE_ENV !== 'production'
 
