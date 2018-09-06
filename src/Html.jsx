b52a5305c6cec326035f0ea23a66837849b96b7e
DanielFGray <DanielFGray@gmail.com>
2018-09-06 00:48:36 -0500

    express helmet

--- src/Html.jsx
+++ src/Html.jsx
@@ -1,65 +1,68 @@
-/* global __APPBASE:false __MOUNT:false */
+/* global __non_webpack_require__:false __APPBASE:false __MOUNT:false */
 /* eslint react/no-danger: off */
 import * as React from 'react'
-import { Helmet } from 'react-helmet'
-import { renderToString, renderToStaticMarkup } from 'react-dom/server'
+import { endsWith } from 'ramda'
+import { partition } from './utils'
+
+const manifest = __non_webpack_require__('./manifest.json')
+
+const [styles, scripts] = partition([
+  endsWith('.css'),
+  endsWith('.js'),
+], Object.values(manifest))
 
 const appBase = __APPBASE === '/' ? '' : __APPBASE
 
 const Html = ({
-  styles = [],
-  scripts = [],
   data,
-  children,
-}) => {
-  const html = renderToString(children)
-  const helmet = Helmet.rewind()
-
-  return renderToStaticMarkup(
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta
-          httpEquiv="x-ua-compatible"
-          content="ie=edge,chrome=1"
+  html,
+  helmet,
+}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        httpEquiv="x-ua-compatible"
+        content="ie=edge,chrome=1"
+      />
+      {helmet.title.toComponent()}
+      {helmet.meta.toComponent()}
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      {helmet.link.toComponent()}
+      {styles.map(css => (
+        <link
+          key={css}
+          rel="stylesheet"
+          type="text/css"
+          href={`${appBase}/${css}`}
         />
-        {helmet.title.toComponent()}
-        {helmet.meta.toComponent()}
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        {helmet.link.toComponent()}
-        {styles.map(css => (
-          <link
-            key={css}
-            rel="stylesheet"
-            href={`${appBase}/${css}`}
-          />
-        ))}
-      </head>
-      <body>
-        <div
-          id={__MOUNT}
-          dangerouslySetInnerHTML={{
-            __html: html,
-          }}
-        />
+      ))}
+    </head>
+    <body>
+      <div
+        id={__MOUNT}
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+      <script
+        type="/text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `window.__INIT_DATA = ${JSON.stringify(data)}`,
+        }}
+      />
+      {scripts.map(js => (
         <script
-          dangerouslySetInnerHTML={{
-            __html: `window.__INIT_DATA = ${JSON.stringify(data)}`,
-          }}
+          key={js}
+          type="text/javascript"
+          src={`${appBase}/${js}`}
         />
-        {scripts.map(js => (
-          <script
-            key={js}
-            src={`${appBase}/${js}`}
-          />
-        ))}
-      </body>
-    </html>,
-  )
-}
-
+      ))}
+    </body>
+  </html>
+)
 
 export default Html
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import * as React from 'react'
 import { StaticRouter } from 'react-router'
 import express from 'express'
 import morgan from 'morgan'
-import { endsWith } from 'ramda'
+import bodyParser from 'body-parser'
+import expressHelmet from 'helmet'
+import { Helmet } from 'react-helmet'
+import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 import Html from './Html'
 import Routes from './client/Routes'
 import Layout from './client/Layout'
-import { partition } from './utils'
 
 const config = __non_webpack_require__('../config')
-const manifest = __non_webpack_require__('./manifest.json')
 
 const {
   appBase,
@@ -23,13 +24,9 @@ const {
 const app = express()
 
 app.use(morgan('dev'))
-
+app.use(expressHelmet())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(publicDir))
-
-const [styles, scripts] = partition([
-  endsWith('.css'),
-  endsWith('.js'),
-], Object.values(manifest))
 
 const getData = () => Promise.resolve({ list: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }] })
 
@@ -45,7 +42,7 @@ app.get('/api*', (req, res) => {
 
 app.get('/', (req, res) => {
   getData().then(data => {
-    const context = data
+    const context = { ...data, query: req.query || {} }
     const children = (
       <StaticRouter basename={appBase} location={req.url} context={context}>
         <Layout>
@@ -53,29 +50,29 @@ app.get('/', (req, res) => {
         </Layout>
       </StaticRouter>
     )
-    try {
-      const html = Html({ styles, scripts, data, children })
-      if (context.url) {
-        res.writeHead(302, {
-          Location: context.url,
-        })
-        res.end()
-      } else {
-        res.end(html)
-      }
-    } catch (e) {
+    let html = renderToString(children)
+    const helmet = Helmet.rewind()
+    html = renderToStaticMarkup(Html({ data, helmet, html }))
+    if (context.url) {
+      res.writeHead(302, {
+        Location: context.url,
+      })
+      res.end()
+    } else {
+      res.end(html)
+    }
+  })
+    .catch(e => {
       console.error(e)
       res.status(501)
         .json(e)
-    }
-  })
+    })
 })
 
 app.listen(port, host, () => console.log(`
   server now running on http://${host}:${port}`))
 
 process.on('uncaughtException', console.error)
-
 process.on('exit', () => console.log('exiting!'))
 process.on('SIGINT', () => console.log('interrupted!'))
 
