4dc0bbffd5cc99cd4cfe0f6f65eb42d7775f9fae
DanielFGray <DanielFGray@gmail.com>
2018-11-29 12:43:59 -0600

    add missing Loading component

---
+++ src/client/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Spinner = () => (
+  <div className="spinner">
+    <div>
+      <span>{`{`}</span>
+      <span>{`}`}</span>
+    </div>
+  </div>
+)
+
+export default Spinner
