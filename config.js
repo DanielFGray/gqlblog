b4d45060358b89dcd8f18d240bb3cb5ac26a82bf
DanielFGray <DanielFGray@gmail.com>
2018-12-14 14:01:55 -0600

    is this production ready?

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
-const appTitle = '[insert title]'
+const appTitle = 'DanielFGray.com'
 const appBase = ''
 const mount = 'root'
 const outputDir = path.resolve('./dist')
 const publicDir = path.resolve('./public')
 const port = process.env.PORT || 8765
-const host = process.env.HOST || 'localhost'
+const host = process.env.HOST || '0.0.0.0'
 const nodeEnv = process.env.NODE_ENV || 'development'
 const devMode = nodeEnv.startsWith('dev')
 
