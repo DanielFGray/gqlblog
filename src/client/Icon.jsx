84da83b954a7be5b70596ddd3810276de4275d7c
DanielFGray <DanielFGray@gmail.com>
2019-01-24 18:32:07 -0600

    add missing files

---
+++ src/client/Icon.jsx
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+const FontAwesomeIcon = React.memo(
+  ({ i, size, className = '', ...props }) => {
+    if (! i) {
+      return 'null'
+    }
+    const {
+      icon: [width, height, , , svgPathData],
+      iconName,
+      prefix,
+    } = i
+    return (
+      <svg
+        aria-hidden="true"
+        className={[
+          'svg-inline--fa',
+          `fa-${iconName}`,
+          `fa-w-${Math.ceil((width / height) * 16)}`,
+          size ? `fa-${size}` : '',
+          className,
+        ].join(' ')}
+        data-icon={iconName}
+        data-prefix={prefix}
+        role="img"
+        viewBox={`0 0 ${width} ${height}`}
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path d={svgPathData} fill="currentColor" />
+      </svg>
+    )
+  },
+)
+
+FontAwesomeIcon.displayName = 'FontAwesomeIcon'
+
+FontAwesomeIcon.propTypes = {
+  className: PropTypes.string,
+  i: PropTypes.object,
+  size: PropTypes.oneOf([
+    'lg',
+    'xs',
+    'sm',
+    '1x',
+    '2x',
+    '3x',
+    '4x',
+    '5x',
+    '6x',
+    '7x',
+    '8x',
+    '9x',
+    '10x',
+  ]),
+}
+
+FontAwesomeIcon.defaultProps = {
+  className: '',
+  i: null,
+  size: null,
+}
+export default FontAwesomeIcon
