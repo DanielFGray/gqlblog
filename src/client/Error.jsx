fe448b441bd20216e1b86e5f21d8f991fa05b577
DanielFGray <DanielFGray@gmail.com>
2019-11-20 18:54:40 -0600

    add Error Boundary component

---
+++ src/client/Error.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+/**
+ * @typedef {object} Props
+ * @prop {React.Component<{error}>} fallback
+ * @prop {React.Component} children
+ * @prop {Function<{error,info}>} didCatch
+ *
+ * @extends {React.Component<Props>}
+ */
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+
+  componentDidCatch(error, info) {
+    if (this.props.didCatch) {
+      this.props.didCatch({ error, info })
+    }
+  }
+
+  render() {
+    const { error } = this.state
+    const { fallback: F, children } = this.props
+    if (error) {
+      return <F error={error} />
+    }
+    return children
+  }
+}
